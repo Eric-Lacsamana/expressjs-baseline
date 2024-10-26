84e088ed53e91ceb3a9b08667e65e0fe10c9aeb1
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-10-26 16:47:38 +0800

    update user entity path

--- src/users/controllers.ts
+++ src/users/controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { User } from '../models/User';
+import { User } from '../entities/User';
 import { AppDataSource } from "../database";
 
 export const getUsers = async (req: Request, res: Response) => {
