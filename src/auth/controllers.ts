e7ba87dfcd108105ad0504e9d16e8db3058812fd
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-10-26 16:38:37 +0800

    intial commit

---
+++ src/auth/controllers.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import bcyrpt from 'bcryptjs';
+
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const { username, password } = req.body;
+
+        const hashedPassword = await bcyrpt.hash(password, 10);
+
+        // write your create user orm here
+
+        res.send({
+            // register user response
+        })
+    } catch (err) {
+        throw new Error('Error registration')
+    }
+    
+}
